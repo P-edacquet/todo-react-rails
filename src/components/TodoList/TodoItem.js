d0676d372ccd6255766febced407fba7e12bd298
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2021-11-18 11:24:25 +0100

    emtions/styled problem fixed (normally)

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/Styles";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete" 
 import { TextareaAutosize, Typography } from "@material-ui/core";
