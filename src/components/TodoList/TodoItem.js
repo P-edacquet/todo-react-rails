63e5554d721f44c7c367b3e402f2eb58498e0115
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2021-11-23 10:03:49 +0100

    TextareaAutosize useless import removed

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -4,7 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete" 
-import { TextareaAutosize, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
