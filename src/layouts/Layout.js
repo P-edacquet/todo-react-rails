bacd63e248b7941e7e077293fb1f8814020a1646
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2021-12-02 09:59:43 +0100

    disconnection done / condition for layout : isPrivate / isLoggedIn updated

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,18 +1,21 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 
 const Layout = ({children, isPrivate = true}) => {
   let history = useHistory()
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
 
   useEffect(() => {
     axios.get('http://localhost:3001/logged_in',
     {withCredentials: true})    
     .then(response => {
-      console.log(response.data)
+      // console.log(response.data)
       if (response.data.logged_in) {
+        setUser(response.data.user)
         setIsLoggedIn(true)
+        // username.current = response.data.user.username
       } else {
         setIsLoggedIn(false)
         if (isPrivate === true) {
@@ -21,7 +24,7 @@ const Layout = ({children, isPrivate = true}) => {
       }
     })
     .catch(error => console.log('api errors:', error))
-  },);
+  }, [history, isPrivate]);
 
   const onClickHandler = () => {
     // App du logout rails
@@ -35,12 +38,14 @@ const Layout = ({children, isPrivate = true}) => {
 
   return (
     <div>
-      { (!isPrivate || isLoggedIn) && <>
+      { isLoggedIn && <>
         <nav>
           <button onClick={onClickHandler}>Logout</button>
         </nav>
-        {children}
-      </>}
+        <h2>Bonjour {user.username} </h2>
+      </> }
+
+      { ( !isPrivate || (isPrivate && isLoggedIn) ) && children }
     </div>
   )
 }
